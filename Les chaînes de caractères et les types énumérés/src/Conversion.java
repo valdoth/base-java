39f881395b00d1ae73728c028d07e9fb36513aec
valdoth <valdotsiarohasina@gmail.com>
2022-06-08 08:31:09 +0300

    Les threads

--- Les chaînes de caractères et les types énumérés/src/Conversion.java
+++ Les chaînes de caractères et les types énumérés/src/Conversion.java
@@ -4,7 +4,7 @@ public class Conversion {
 	public static void main(String[] args) {
 		
 		// conversion d'un type primitif en une chaine
-		int n = 3948;
+		int n = 3958;
 		String ch = String.valueOf(n);
 		System.out.println(n + " -> " + ch);
 		double b = -65.780;
