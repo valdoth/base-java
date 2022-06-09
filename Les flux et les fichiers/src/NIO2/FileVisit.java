dd163cf6042352e69ed56575d4bdb1f0e46fc1ab
valdoth <valdotsiarohasina@gmail.com>
2022-06-09 12:11:38 +0300

    Les flux et les fichiers

--- Les flux et les fichiers/src/NIO2/FileVisit.java
+++ Les flux et les fichiers/src/NIO2/FileVisit.java
@@ -1,14 +1,31 @@
 package NIO2;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileVisit {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		Path p = Paths.get()
+		Path p = Paths.get("/home/ndimbiarisoa/Desktop/pratique java/programmer en java");
+		AffichRepert ar = new AffichRepert();
+		Files.walkFileTree(p, ar);
 		
 	}
 
+}
+
+class AffichRepert extends SimpleFileVisitor<Path> {
+	public FileVisitResult visitFile(Path fich, BasicFileAttributes attr) {
+		// on entre ici a chaque fichier visite
+		if (attr.isRegularFile()) {
+			System.out.println(fich.getFileName());
+		}
+		return FileVisitResult.CONTINUE;
+	}
 }
