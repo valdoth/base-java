3a0bc281238724f9587fd19f02e44aba21b3d7e6
valdoth <valdotsiarohasina@gmail.com>
2022-06-09 12:02:48 +0300

    Les flux et les fichiers

---
+++ Les flux et les fichiers/src/NIO2/FileVisit.java
@@ -0,0 +1,14 @@
+package NIO2;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileVisit {
+
+	public static void main(String[] args) {
+		
+		Path p = Paths.get()
+		
+	}
+
+}
